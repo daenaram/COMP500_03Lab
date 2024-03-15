738d72182214f52bc6a3f2c1d6c248b194ab0a45
daenaram <157431231+daenaram@users.noreply.github.com>
2024-03-15 22:37:45 +1300

    Update q18.c

--- q18.c
+++ q18.c
@@ -1,1 +1,28 @@
+// still doesnt work
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159f
+
+int main() {
+    float hypotenuse, angle_degrees;
+    float opposite, adjacent;
+    float angle_radians;
+    
+    printf("What is the angle in degrees?\n");
+    scanf("%f", &angle_degrees);
+
+    printf("What is the length of the hypotenuse?\n\n");
+    scanf("%f", &hypotenuse);
+
+    angle_radians = angle_degrees * (PI / 180.0f);
+
+    opposite = sin(angle_radians) * hypotenuse;
+    adjacent = cos(angle_radians) * hypotenuse;
+
+    printf("The length of the opposite is: %f\n", opposite);
+    printf("The length of the adjacent is: %f\n", adjacent);
+
+    return 0;
+}
 
